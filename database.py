848efb88cdcf05dd5349370158089b817849b81c
cmwuaan <minhquanrbpk@gmail.com>
2023-11-20 14:57:30 +0700

    first commit

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# URL_DATABASE = 'postgresql://postgres:QuanLDG71.@localhost:5432/postgres'
+URL_DATABASE = 'mysql+pymysql://<your_host_name>:<your_password>@localhost:<your_port>/<your_db>'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
